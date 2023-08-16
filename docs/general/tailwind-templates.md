d4fdf8e9ee3907539c5d85ac4696e24d963c4c76
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-08-16 17:49:26 +0100

    Tailwind headline elements designed, class info noted in tailwind-templates doc

---
+++ docs/general/tailwind-templates.md
@@ -0,0 +1,10 @@
+# Tailwind Templates
+
+## Headings
+```
+main heading
+<h1 className="font-semibold text-7xl m-4 p-4 text-center"></h1>
+
+subheading
+<h2 className="font-medium text-4xl m-3 p-3 text-center">This is the sub heading...</h2>
+```
---
+++ src/app/tailwind-templates/page.tsx
@@ -0,0 +1,8 @@
+export default function TailwindTemplates() {
+    return (
+        <div>
+            <a href="./"><h1 className="font-semibold text-7xl m-4 p-4 text-center">Hello from new-page</h1></a>
+            <h2 className="font-medium text-4xl m-3 p-3 text-center">This is the sub heading...</h2>
+        </div>
+    )
+}
