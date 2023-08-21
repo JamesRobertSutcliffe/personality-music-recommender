bd02a6e06ca774788998f2e9ccf44350fd900afc
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-08-21 20:29:28 +0100

    classname prop added to subheading component

--- src/app/components/Subheading.tsx
+++ src/app/components/Subheading.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from "react"
 
-function Subheading({ children }: { children: ReactNode }) {
+function Subheading({ children, className }: { children: ReactNode, className?: string }) {
     return (
         <>
-            <h2 className="font-medium text-5xl m-2 p-2 text-center text-awhite">{children}</h2>
+            <h2 className={`font-medium text-5xl m-2 p-2 text-center text-awhite ${className}`}>{children}</h2>
         </>
     )
 }
