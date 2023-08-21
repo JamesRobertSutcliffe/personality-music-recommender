f6658cbddde4fdddf0cc791e8ac0fb60a8169ce8
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-08-21 20:27:39 +0100

    colour prop removed from BTN / onclick prop passed and tested

--- src/app/components/Btn.tsx
+++ src/app/components/Btn.tsx
@@ -1,16 +1,15 @@
 import { ReactNode, MouseEventHandler } from "react";
 
 interface BtnProps {
-  children: ReactNode;
-  colour: string;
-  className?: string;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
+    children: ReactNode;
+    className?: string;
+    onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
-function Btn({ children, colour, className, onClick }: BtnProps) {
+function Btn({ children, className, onClick }: BtnProps) {
     return (
         <>
-            <button className={`rounded-none bg-${colour} m-auto p-3 font-medium text-lg text-awhite justify-center ${className}`} onClick={onClick} >{children}</button>
+            <button className={`rounded-none m-auto p-3 font-medium text-lg text-awhite justify-center ${className}`} onClick={onClick} >{children}</button>
         </>
     )
 }
