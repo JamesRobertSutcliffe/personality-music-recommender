3902f1e527002af7c2318e7b98bfa5bcd3ddb711
patdel0 <zefiru@protonmail.com>
2023-08-21 10:59:24 +0100

    Use `interface` to separate types from component
    Type declarations were starting to get messy when inline with the
    component.
    The code has been refactored by separating types into distinct interface for enhanced
    readability and maintainability.

--- src/app/components/Btn.tsx
+++ src/app/components/Btn.tsx
@@ -1,6 +1,13 @@
 import { ReactNode, MouseEventHandler } from "react";
 
-function Btn({ children, colour, className, onClick }: { children: ReactNode, colour: string, className?: string,  onClick?: MouseEventHandler<HTMLButtonElement> }) {
+interface BtnProps {
+  children: ReactNode;
+  colour: string;
+  className?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+function Btn({ children, colour, className, onClick }: BtnProps) {
     return (
         <>
             <button className={`rounded-none bg-${colour} m-auto p-3 font-medium text-lg text-awhite justify-center ${className}`} onClick={onClick} >{children}</button>
