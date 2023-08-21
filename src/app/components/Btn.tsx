992dcc244c2e01702dd53af7e4e6dd6919168755
patdel0 <zefiru@protonmail.com>
2023-08-21 10:57:28 +0100

    Handle onClick events on `Btn`

--- src/app/components/Btn.tsx
+++ src/app/components/Btn.tsx
@@ -1,9 +1,9 @@
-import { ReactNode } from "react";
+import { ReactNode, MouseEventHandler } from "react";
 
-function Btn({ children, colour, className }: { children: ReactNode, colour: string, className?: string }) {
+function Btn({ children, colour, className, onClick }: { children: ReactNode, colour: string, className?: string,  onClick?: MouseEventHandler<HTMLButtonElement> }) {
     return (
         <>
-            <button className={`rounded-none bg-${colour} m-auto p-3 font-medium text-lg text-awhite justify-center ${className}`}>{children}</button>
+            <button className={`rounded-none bg-${colour} m-auto p-3 font-medium text-lg text-awhite justify-center ${className}`} onClick={onClick} >{children}</button>
         </>
     )
 }
