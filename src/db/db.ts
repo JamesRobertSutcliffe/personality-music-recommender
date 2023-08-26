f4c46de4237fe240d31decc1b2514ec78f91df31
patdel0 <zefiru@protonmail.com>
2023-08-26 23:41:35 +0100

    Add conditional for SSL handling
    If an environment requires the SSL encryption and authentication protocol, the optional DB_SSL environment variable
    should be set to "true"

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,16 +2,15 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 export const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-  ssl: {
+  ssl: process.env.DB_SSL ? {
     rejectUnauthorized: false,
-  },
+  } : false,
 });
 
 /**
