f9a33ed99605e67879ef886c4be4fa1dd5e42889
patdel0 <zefiru@protonmail.com>
2023-08-27 01:27:13 +0100

    Remove JSDocs to leverage Typescript types

--- src/db/db.ts
+++ src/db/db.ts
@@ -16,8 +16,8 @@ export const pool = new Pool({
 /**
  * The `query` function is a helper function that executes a SQL query using the provided text and optional parameters.
  *
- * @param {string} text - The SQL query string to execute.
- * @param {any[]} [params] - Optional array of parameter values to substitute in the query.
- * @returns {Promise<any>} - A promise that resolves to the result of the query execution.
+ * @param text - The SQL query string to execute.
+ * @param [params] - Optional array of parameter values to substitute in the query.
+ * @returns - A promise that resolves to the result of the query execution.
  */
 export const query = (text: string, params?: any[]) => pool.query(text, params);
