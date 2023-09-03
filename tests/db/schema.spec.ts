56a1e55e55f39c23e36cf4fa7ca25ea39a4b6019
patdel0 <zefiru@protonmail.com>
2023-09-03 17:09:03 +0100

    Add unit tests for schema creation

---
+++ tests/db/schema.spec.ts
@@ -0,0 +1,26 @@
+import { query } from '../../src/db/db';
+
+async function tableExists(tableName: string) {
+  const queryResult = await query(`
+    SELECT EXISTS (
+      SELECT FROM 
+        information_schema.tables 
+      WHERE 
+        table_schema = 'public' AND 
+        table_name = '${tableName}'
+    );
+  `);
+  const existsValue = queryResult.rows[0].exists;
+  return existsValue;
+}
+
+const tableNames: string[] = ['personality_types', 'users', 'favorites', 'recommendations'];
+describe('Table creation', () => {
+  tableNames.forEach(tableName => {
+    it(`should create the ${tableName} table`, async () => {
+      const tableExistsValue = await tableExists(tableName);
+      expect(tableExistsValue).toEqual(true);
+    });
+  });
+});
+
