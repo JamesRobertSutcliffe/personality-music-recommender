3b39cb60f4af7625acfb3c1c8bc8fd8d70a28586
patdel0 <zefiru@protonmail.com>
2023-09-03 17:06:04 +0100

    Create schema for the required tables

---
+++ src/db/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS personality_types (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255),
+  password VARCHAR(255),
+  personality_type_id INT REFERENCES personality_types(id),
+  other_info TEXT
+);
+
+CREATE TABLE IF NOT EXISTS favorites (
+  user_id INT REFERENCES users(id),
+  song_id INT,
+  PRIMARY KEY (user_id, song_id)
+);
+
+CREATE TABLE IF NOT EXISTS recommendations (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  recommended_song_id INT
+);
+
