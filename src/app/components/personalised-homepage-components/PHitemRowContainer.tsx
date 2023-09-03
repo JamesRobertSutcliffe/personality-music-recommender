7da6dba3c8282ffae1ef8da11a1cc3911168c21b
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-09-03 17:03:47 +0100

    These components have been renamed and therefore these can be removed from project.

--- src/app/components/personalised-homepage-components/PHitemRowContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-function PHitemRowContainer({ children, className }: { children: ReactNode, className?: String }) {
-    return (
-        <div className={`grid-rows-3 max-h-[83vh] overflow-auto ${className}`}>{children}</div>
-    )
-}
-
-export default PHitemRowContainer;
