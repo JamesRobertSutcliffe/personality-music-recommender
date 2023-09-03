e71a65339c9553959a33d8ad0f3c63808158cf1f
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-09-03 17:04:48 +0100

    Logo uploaded and added to navbar / homepage

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -7,6 +7,7 @@ import Btn from "../components/Btn"
 import AnchorBtn from "../components/AnchorBtn"
 import Image from "next/image"
 import HomepageImage from "../../../public/images/homepage_holder.png"
+import Logo from "../../../public/images/logo.png"
 
 export default function Homepage() {
 
@@ -18,16 +19,24 @@ export default function Homepage() {
             <Subheading className="text-agrey">
                 Discover new music based on your personality type
             </Subheading>
-            <Image src={HomepageImage}
-                className="m-auto p-5"
-                width={800}
-                height={800}
-                alt="Personality Homepage Image">
-            </Image>
+            <div className="flex justify-center">
+                <Image src={HomepageImage}
+                    className="m-1 p-1 inline"
+                    width={800}
+                    height={800}
+                    alt="Personality Homepage Image">
+                </Image>
+                <Image src={Logo}
+                    className="m-1 p-1 inline"
+                    width={400}
+                    height={400}
+                    alt="Personality Homepage Image">
+                </Image>
+            </div>
             <Paragraph>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolorum debitis reprehenderit iste consectetur! Aut,
                 optio sit. Rerum quis explicabo et provident officiis autem amet neque repellendus? Earum, laborum sunt. Lorem ipsum
-                dolor sit amet consectetur adipisicing elit.
+                dolor sit amet
             </Paragraph>
             <div className="flex m-5">
                 <AnchorBtn link={"login"} className="bg-ablue">Take the Test!</AnchorBtn>
