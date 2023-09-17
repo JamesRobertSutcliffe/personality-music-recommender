71e8865a6b24b55a870a402689cf7648b9f03a0f
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-09-17 20:06:05 +0100

    Unfortunately I could not get this component to work with typescript, having trouble getting TS to accept the onclick function passed down.
    I will make another branch to fix this specifically hopefully after a few pointers. JS component is working as needed.

--- src/app/components/personalised-homepage-components/ItemCard.js
+++ src/app/components/personalised-homepage-components/ItemCard.js
@@ -1,17 +1,17 @@
-import Link from "next/link";
-import Image from "next/image";
-import { ReactNode } from "react"
+'use client'
+import { ReactNode } from "react";
 
-function ItemCard({ children, title, img }: { children?: ReactNode, title?: String, img?: string }) {
+function ItemCard({ children, title, img, trackID, setPlaybackID }) {
+
     return (
-        <Link className="hover:bg-gray-700 delay-50 duration-100 bg-ablack p-5 rounded-lg min-w-[10vw] group p-2 m-2" href="">
+        <div className="hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-700 delay-50 duration-100 bg-ablack p-5 rounded-lg min-w-[10vw] group p-2 m-2" id={trackID} onClick={setPlaybackID}>
             <img src={img}
                 className="w-fulls rounded shadow"
                 alt="Personality Homepage Image">
             </img>
             <h3 className="text-gray-200 font-bold mt-5 text-center">{title}</h3>
             <p className="text-gray-400 font-light mt-2 text-xs text-center">{children}</p>
-        </Link>
+        </div>
     )
 }
 
