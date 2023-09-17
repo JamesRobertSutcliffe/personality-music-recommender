6a536764793a01b40aac8ef2d58c9da04adbf5e1
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-09-17 20:08:40 +0100

    Functionality added to allow playback. When users click on cards playback should occur. If user select albums or playlists they can go use previous or next songs using appropriate buttons.

--- src/app/personalised-homepage/page.js
+++ src/app/personalised-homepage/page.js
@@ -1,12 +1,13 @@
 'use client'
 import Aside from "../components/personalised-homepage-components/Aside";
-import ItemCard from "../components/personalised-homepage-components/ItemCard";
 import ItemContainer from "../components/personalised-homepage-components/ItemContainer";
 import ItemRow from "../components/personalised-homepage-components/ItemRow";
 import ItemRowContainer from "../components/personalised-homepage-components/ItemRowContainer";
 import Bg from "../components/personalised-homepage-components/Bg";
 import { useEffect, useState } from "react";
 import { fetchArtistAlbum, fetchProfile, fetchUserPlaylists, fetchUserTracks, fetchArtistTrack } from "../hooks/spotify/spotify_hooks";
+import SpotifyPlayer from 'react-spotify-web-playback';
+import ItemCard from "../components/personalised-homepage-components/ItemCard";
 
 function PersonalisedHomepage() {
 
@@ -22,6 +23,12 @@ function PersonalisedHomepage() {
     const [albums, setAlbums] = useState([]);
     const [tracks, setTracks] = useState([]);
     const [playlists, setPlaylists] = useState([]);
+    const [userTopArray, setUserTopArray] = useState([]);
+
+    // Variables / state used to control playback
+    const [playTrack, setPlayTrack] = useState("");
+    const [playerType, setPlayerType] = useState("");
+    const [playback, setPlayback] = useState(false);
 
     // Pulls in placeholder data from a couple of areas of Spotify API
     useEffect(() => {
@@ -30,21 +37,43 @@ function PersonalisedHomepage() {
             setUserEmail(res.email);
             setUserImage(res.images?.[1]?.url);
         });
+        fetchUserTracks(accessToken).then((res) => setUserTopArray(res.items))
         fetchArtistAlbum(accessToken).then((res) => setAlbums(res.items));
         fetchArtistTrack(accessToken).then((res) => setTracks(res.tracks));
         fetchUserPlaylists(accessToken).then((res) => setPlaylists(res.playlists?.items));
     }, []);
 
-    console.log(tracks)
-
     // Useful data for ML / backend //
     // User top tracks is an array of users top played track IDs
-    const userTopTracks = tracks?.map((track) => {
+    const userTopTracks = userTopArray?.map((track) => {
         return track.id
     })
+
+    console.log(userTopTracks);
     // User Email - could be used as a unique identifier to store in DB and match to personality type
     const [userEmail, setUserEmail] = useState("");
     console.log(userEmail)
+
+
+    // Below functions play tracks / albums / playlists on click
+
+    function setTrackId(e) {
+        setPlayTrack(e.currentTarget.id);
+        setPlayerType("track");
+        setPlayback(true);
+    }
+
+    function setAlbumId(e) {
+        setPlayTrack(e.currentTarget.id);
+        setPlayerType("album");
+        setPlayback(true);
+    }
+
+    function setPlaylistId(e) {
+        setPlayTrack(e.currentTarget.id);
+        setPlayerType("playlist");
+        setPlayback(true);
+    }
 
     return (
         <Bg>
@@ -54,22 +83,30 @@ function PersonalisedHomepage() {
                 <ItemRowContainer>
                     <ItemRow title="Recommended Songs">
                         {tracks?.map((track, index) => {
-                            return <ItemCard key={`song${index}`} title={track.name} img={track.album.images[1].url}>{track.artists[0].name}</ItemCard>
+                            return <ItemCard key={`song${index}`} trackID={track.id} title={track.name} img={track.album.images[1].url} setPlaybackID={setTrackId}>{track.artists[0].name}</ItemCard>
                         })}
                     </ItemRow>
                     <ItemRow title="Recommended Albums">
                         {albums?.map((album, index) => {
-                            return <ItemCard key={`album${index}`} title={album.name} img={album.images[1].url}>{album.artists[0].name}</ItemCard>
+                            return <ItemCard key={`album${index}`} trackID={album.id} title={album.name} img={album.images[1].url} setPlaybackID={setAlbumId}>{album.artists[0].name}</ItemCard>
                         })}
                     </ItemRow>
                     <ItemRow title="Recommended Playlists">
                         {playlists?.map((playlist, index) => {
-                            return <ItemCard key={`playlist${index}`} title={playlist.name} img={playlist.images[0].url}>{playlist.description}</ItemCard>
+                            return <ItemCard key={`playlist${index}`} trackID={playlist.id} title={playlist.name} img={playlist.images[0].url} setPlaybackID={setPlaylistId}>{playlist.description}</ItemCard>
                         })}
                     </ItemRow>
                 </ItemRowContainer>
             </ItemContainer>
+            <div className="min-w-[100vw]">
+                <SpotifyPlayer
+                    token={accessToken}
+                    uris={[`spotify:${playerType}:${playTrack}`]}
+                    play={playback}>
+                </SpotifyPlayer>
+            </div>
         </Bg>
+
     )
 }
 
