ad59e6a8b23bb896a7900b1bb7638d4136f4b61e
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-09-25 23:18:28 +0100

    ItemCard component reconverted to TSX  / react node import remove

--- src/app/components/personalised-homepage-components/ItemCard.tsx
+++ src/app/components/personalised-homepage-components/ItemCard.tsx
@@ -1,7 +1,14 @@
 'use client'
-import { ReactNode } from "react";
 
-function ItemCard({ children, title, img, trackID, setPlaybackID }) {
+interface ICard {
+    children?: any;
+    title: string;
+    img: string;
+    trackID: string;
+    setPlaybackID: (e: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+function ItemCard({ children, title, img, trackID, setPlaybackID }: ICard) {
 
     return (
         <div className="hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-700 delay-50 duration-100 bg-ablack p-5 rounded-lg min-w-[10vw] group p-2 m-2" id={trackID} onClick={setPlaybackID}>
