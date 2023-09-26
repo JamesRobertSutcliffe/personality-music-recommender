d5b093ee1029c0a67ab3e1409d276809457d1f4c
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-09-26 19:13:35 +0100

    Logo component built to render while waiting profile info data fetch. Just heading "Loading" text as placeholder for now. I will look to update this to some kind of animation going forwards.

---
+++ src/app/components/personalised-homepage-components/Loading.tsx
@@ -0,0 +1,9 @@
+import Heading from "../Heading";
+
+export default function () {
+    return (
+        <div className="text-center justify-center align-center">
+            <Heading>Loading</Heading>
+        </div>
+    )
+}
