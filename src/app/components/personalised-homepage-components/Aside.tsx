8629da4690e0dffa4e68f0023d1aae4bc12e373d
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-09-26 19:09:41 +0100

    Aside component updated to ensure image fits in it's container.

--- src/app/components/personalised-homepage-components/Aside.tsx
+++ src/app/components/personalised-homepage-components/Aside.tsx
@@ -6,7 +6,7 @@ function Aside({ username, personalityType, profileImage }: { username: String, 
                     <div className="row-span-1 bg-ablack p-1 m-2 min-h-[40vh]">
                         <h3 className="text-xl text-center">Logged in as {username}</h3>
                         <div className="flex items-center justify-center align-center m-4">
-                            <img src={profileImage} alt="Users Profile Image"></img>
+                            <img src={profileImage} alt="Users Profile Image" className="max-w-[300px] max-h-[300px]"></img>
                         </div>
                     </div>
                     <div className="row-span-1 bg-ablack p-1 m-2 min-h-[40vh] flex justify-center items-center">
