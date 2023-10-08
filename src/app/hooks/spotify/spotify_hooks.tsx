8cd35a28168fbace12b48f19993b25c1fca307c1
patdel0 <zefiru@protonmail.com>
2023-10-08 19:12:35 +0100

    Fetch the recommend algorithm's tracks

--- src/app/hooks/spotify/spotify_hooks.tsx
+++ src/app/hooks/spotify/spotify_hooks.tsx
@@ -12,11 +12,25 @@ export async function fetchArtistAlbum(token: string) {
     return await result.json();
 }
 
-export async function fetchArtistTrack(token: string) {
-    const result = await fetch("https://api.spotify.com/v1/artists/4ePunOqQbOYoQwd1298g3Z/top-tracks?country=es", {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
+export async function fetchRecommendedTracks(token: string, personalityType: string) {
+    if(!personalityType) return;
+
+    const apiResponse = await fetch(`https://patdel0-personality-music-recommender.hf.space/recommend?mbti_type=${personalityType}&num_of_songs=10`);
+    const trackSuggestions = await apiResponse.json();
+
+    const trackFetchPromises = trackSuggestions.map((suggestion: {track_id: string}) => fetchTrackData(token, suggestion.track_id));
+    const fetchedTracks = await Promise.all(trackFetchPromises);
+
+    return fetchedTracks;
+}
+
+export async function fetchTrackData(token: string, trackId: string) {
+    const result = await fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` }
     });
-    return await result.json();
+    const data = await result.json();
+    return data;
 }
 
 export async function fetchUserTracks(token: string) {
@@ -31,4 +45,4 @@ export async function fetchUserPlaylists(token: string) {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
     return await result.json();
-} 
+}
