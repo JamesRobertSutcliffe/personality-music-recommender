fe8d86f6ee82610adc9ed66debe62ec8382ddf21
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-10-10 21:21:11 +0100

    Hooks to fetch albums from Spotify API added, designed to take numerous album IDs which are taken from the personalised track data that is fetched.
    Functions reordered and comments added to tidy up page.

--- src/app/hooks/spotify/spotify_hooks.tsx
+++ src/app/hooks/spotify/spotify_hooks.tsx
@@ -1,3 +1,5 @@
+// Fetch User Profile Data
+
 export async function fetchProfile(token: string) {
     const result = await fetch("https://api.spotify.com/v1/me/", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
@@ -5,20 +7,23 @@ export async function fetchProfile(token: string) {
     return await result.json();
 }
 
-export async function fetchArtistAlbum(token: string) {
-    const result = await fetch("https://api.spotify.com/v1/artists/4ePunOqQbOYoQwd1298g3Z/albums", {
+// Fetch Users Top Tracks
+
+export async function fetchUserTracks(token: string) {
+    const result = await fetch("https://api.spotify.com/v1/me/top/tracks", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
     return await result.json();
 }
 
+// Fetch Personality Type Tracks
+
 export async function fetchRecommendedTracks(token: string, personalityType: string) {
-    if(!personalityType) return;
+    if (!personalityType) return;
 
-    const apiResponse = await fetch(`https://patdel0-personality-music-recommender.hf.space/recommend?mbti_type=${personalityType}&num_of_songs=10`);
+    const apiResponse = await fetch(`https://patdel0-personality-music-recommender.hf.space/recommend?mbti_type=${personalityType}&num_of_songs=20`);
     const trackSuggestions = await apiResponse.json();
-
-    const trackFetchPromises = trackSuggestions.map((suggestion: {track_id: string}) => fetchTrackData(token, suggestion.track_id));
+    const trackFetchPromises = trackSuggestions.map((suggestion: { track_id: string }) => fetchTrackData(token, suggestion.track_id));
     const fetchedTracks = await Promise.all(trackFetchPromises);
 
     return fetchedTracks;
@@ -33,11 +38,15 @@ export async function fetchTrackData(token: string, trackId: string) {
     return data;
 }
 
-export async function fetchUserTracks(token: string) {
-    const result = await fetch("https://api.spotify.com/v1/me/top/tracks", {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
+// Fetch Personality Type Albums
+
+export async function fetchAlbumData(token: string, albumId: string) {
+    const result = await fetch(`https://api.spotify.com/v1/albums?ids=${albumId}`, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` }
     });
-    return await result.json();
+    const data = await result.json();
+    return data;
 }
 
 export async function fetchUserPlaylists(token: string) {
